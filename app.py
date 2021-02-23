dc4af269759fffec57f188d46fdf89656db32f14
taeheelee90 <taehee.lee90@gmail.com>
2021-02-23 21:26:18 +0200

    Updated to show URL

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 def studnt_list():
     # Get student name and git link from csv file
     data_list = []
-    final_results=[]
-    final_links = []
+    final_results=[]    
 
     # Read student information from csv and append data to data_lists
     with open (r"./k2021Project.csv", encoding='utf-8-sig') as f:
@@ -18,15 +17,21 @@ def studnt_list():
             data_list.append(c)
 
     # Iterate data_list to check last commit to the repository
-    for d in data_list:
+    for d in data_list:        
         if (d['Git'] == ''):
-            final_results.append(d['Name'] + ", please submit repository!")
+            result = {'name' : d['Name'], 'last_commit' : 'n/a', 'url': 'no repository'}
+            final_results.append(result)         
         else:
             gitUrl = d['Git'].replace("https://github.com/", "").split('/')
             checker.gitLastCommit(d['Name'], gitUrl[0], gitUrl[1], final_results)     
-      
-    return render_template('result.html', result = final_results)
+    
+    return render_template('result.html', 
+                            result = final_results,
+                            total = len(final_results))
 
+@app.route('/no')
+def no_repository():
+    return "repository not found"
 
 if __name__ == "__main__":
     app.run(host = '127.0.0.1', port = 5000, debug = True)
--- commit_checker.py
+++ commit_checker.py
@@ -5,7 +5,7 @@ import requests
 def gitLastCommit(name, username, repository, final_results):
     #url & headers
     url = f"https://api.github.com/repos/{username}/{repository}"
-    request_headers={'Authorization' : 'token ff3688095f5a07de567c366e97b9e9ba085af65f'}
+    request_headers={'Authorization' : 'token WRITEYOURTOKENHERE'}
 
     #request to json
     repo_data = requests.get(url, headers=request_headers).json()
@@ -20,15 +20,16 @@ def gitLastCommit(name, username, repository, final_results):
     now = datetime.strptime(now, '%Y-%m-%d %H:%M:%S')
 
     if ('message' in repo_data):
-        final_results.append(name + ", repository not found!")
+        result = {'name' : name, 'last_commit' : 'n/a', 'url': 'no repository'}
+        final_results.append(result)  
     else: 
     # convert string to datetime (in UTC+2: HELSINKI)
         last_pushed_date = datetime.strptime(pushed_at_str, '%Y-%m-%dT%H:%M:%SZ') + timedelta(hours=2)
         daysAgo = (now - last_pushed_date).days
 
         if (daysAgo == 0): # if last update was within last 24hrs
-            result = name + ", Last commit was today. "
+            result = {'name' : name, 'last_commit' : 'today', 'url': url_to_repository}
         else:
-            result = name + ", Last commit was " + str(daysAgo) + " days ago. "
+            result = {'name' : name, 'last_commit' : str(daysAgo) + ' days ago', 'url': url_to_repository}
       
         final_results.append(result)   
