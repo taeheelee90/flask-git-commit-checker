463dd863561a5769245987845e20240bf87c1f98
taeheelee90 <taehee.lee90@gmail.com>
2021-03-15 13:17:31 +0200

    Add project repository

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import commit_checker as checker
 app = Flask(__name__)
 
 @app.route("/")
-def studnt_list():
+def project_list():
     # Get student name and git link from csv file
     data_list = []
     final_results=[]    
@@ -25,7 +25,31 @@ def studnt_list():
             gitUrl = d['Git'].replace("https://github.com/", "").split('/')
             checker.gitLastCommit(d['Name'], gitUrl[0], gitUrl[1], final_results)     
     
-    return render_template('result.html', 
+    return render_template('project_result.html', 
+                            result = final_results,
+                            total = len(final_results))
+@app.route("/book-store")
+def bookstore_list():
+    # Get student name and git link from csv file
+    data_list = []
+    final_results=[]    
+
+    # Read student information from csv and append data to data_lists
+    with open (r"./k2021Bookstore.csv", encoding='utf-8-sig') as f:
+        reader = csv.DictReader(f)
+        for c in reader:
+            data_list.append(c)
+
+    # Iterate data_list to check last commit to the repository
+    for d in data_list:        
+        if (d['Git'] == ''):
+            result = {'name' : d['Name'], 'last_commit' : 'n/a', 'url': 'no repository'}
+            final_results.append(result)         
+        else:
+            gitUrl = d['Git'].replace("https://github.com/", "").split('/')
+            checker.gitLastCommit(d['Name'], gitUrl[0], gitUrl[1], final_results)     
+    
+    return render_template('bookstore_result.html', 
                             result = final_results,
                             total = len(final_results))
 
